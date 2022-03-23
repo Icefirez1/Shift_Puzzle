7e51cf78de9702082ac1331b59fd7f38113a00c6
lucror1 <paulw2022@gmail.com>
2022-03-23 13:48:49 -0400

    Restyled Permutation.java to match other files

--- Permutation.java
+++ Permutation.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class Permutation {
+public class Permutation
+{
     // Stores the number of objects being acted upon
     private int numObjects;
 
@@ -21,19 +22,23 @@ public class Permutation {
      * @return a Permutation on n objects with numSwaps swaps
      * @throws IllegalArgumentException if n or numSwaps is negative
      */
-    public static Permutation randomPermutation(int n, int numSwaps) {
+    public static Permutation randomPermutation(int n, int numSwaps)
+    {
         // Validate n and numSwaps
-        if (n < 0) {
+        if (n < 0)
+        {
             throw new IllegalArgumentException("n must be non-negative");
         }
-        if (numSwaps < 0) {
+        if (numSwaps < 0)
+        {
             throw new IllegalArgumentException("numSwaps must be non-negative");
         }
 
         // Generate the random list
         Random rand = new Random();
         int[] indices = new int[2*numSwaps];
-        for (int i = 0; i < 2*numSwaps; i++) {
+        for (int i = 0; i < 2*numSwaps; i++)
+        {
             indices[i] = rand.nextInt(n);
         }
 
@@ -51,12 +56,15 @@ public class Permutation {
      * @throws IllegalArgumentException if n or minSwaps is negative or if
      * maxSwaps is less than or equal to minSwaps
      */
-    public static Permutation randomPermutation(int n, int minSwaps, int maxSwaps) {
+    public static Permutation randomPermutation(int n, int minSwaps, int maxSwaps)
+    {
         // Validate n, minSwaps, and maxSwaps
-        if (n < 0) {
+        if (n < 0)
+        {
             throw new IllegalArgumentException("n must be non-negative");
         }
-        if (minSwaps < 0) {
+        if (minSwaps < 0)
+        {
             throw new IllegalArgumentException("minSwaps must be non-negative");
         }
         if (maxSwaps <= minSwaps) {
@@ -74,9 +82,11 @@ public class Permutation {
     // indices is an even length array of ints
     // Each pair of ints reperesents a swap
     // Ex. {3, 4, 5, 6} represents swapping 3 and 4, then 5 and 6
-    private Permutation(int n, int[] indices) {
+    private Permutation(int n, int[] indices)
+    {
         // Make sure indicies is even
-        if (indices.length % 2 != 0) {
+        if (indices.length % 2 != 0)
+        {
             throw new IllegalArgumentException("indices must have an even length");
         }
         
@@ -89,7 +99,8 @@ public class Permutation {
      * Returns whether or not this permutation is even.
      * @return true if the permutation is even, false otherwise
      */
-    public boolean isEven() {
+    public boolean isEven()
+    {
         return isEven;
     }
 
@@ -99,9 +110,11 @@ public class Permutation {
      * permutation
      * @throws IllegalArgumentException if list has the wrong length
      */
-    public <T> void applyToList(List<T> list) {
+    public <T> void applyToList(List<T> list)
+    {
         // Validate the list of the length
-        if (list.size() != numObjects) {
+        if (list.size() != numObjects)
+        {
             throw new IllegalArgumentException("list must be the same length as the permutation");
         }
 
@@ -109,7 +122,8 @@ public class Permutation {
         // Iterate over every other index
         // Swap the current and next indices
         // Ex. 2,3,6,5 would swap 2 and 3, then 6 and 5
-        for (int i = 0; i < indices.length; i += 2) {
+        for (int i = 0; i < indices.length; i += 2)
+        {
             swap(list, indices[i], indices[i+1]);
         }
     }
@@ -120,9 +134,11 @@ public class Permutation {
      * permutation
      * @throws IllegalArgumentException if array has the wrong length
      */
-    public <T> void applyToArray(T[] array) {
+    public <T> void applyToArray(T[] array)
+    {
         // Validate the array length
-        if (array.length != numObjects) {
+        if (array.length != numObjects)
+        {
             throw new IllegalArgumentException("array must be the same length as the permutation");
         }
 
@@ -130,7 +146,8 @@ public class Permutation {
         // Iterate over every other index
         // Swap the current and next indices
         // Ex. 2,3,6,5 would swap 2 and 3, then 6 and 5
-        for (int i = 0; i < indices.length; i += 2) {
+        for (int i = 0; i < indices.length; i += 2)
+        {
             swap(array, indices[i], indices[i+1]);
         }
     }
@@ -144,15 +161,18 @@ public class Permutation {
      * @throws IllegalArgumentException if array has the wrong number of
      * items
      */
-    public <T> void applyTo2DArray(T[][] array) {
+    public <T> void applyTo2DArray(T[][] array)
+    {
         // Validate the array size
-        if (array.length * array[0].length != numObjects) {
+        if (array.length * array[0].length != numObjects)
+        {
             throw new IllegalArgumentException("array must have the same number of items as the permutation");
         }
 
         // Apply the permutation
         // Convert the 1D indices into 2D indices
-        for (int i = 0; i < indices.length; i += 2) {
+        for (int i = 0; i < indices.length; i += 2)
+        {
             int a = indices[i];
             int b = indices[i+1];
             int len = array.length;
@@ -162,21 +182,24 @@ public class Permutation {
     }
 
     // Swap the ith and jth elements in list
-    private <T> void swap(List<T> list, int i, int j) {
+    private <T> void swap(List<T> list, int i, int j)
+    {
         T temp = list.get(i);
         list.set(i, list.get(j));
         list.set(j, temp);
     }
 
     // Swap the ith and jth elements in array
-    private <T> void swap(T[] array, int i, int j) {
+    private <T> void swap(T[] array, int i, int j)
+    {
         T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 
     // Swap the ith and jth elements in a 2D array
-    private <T> void swap(T[][] array, int ix, int iy, int jx, int jy) {
+    private <T> void swap(T[][] array, int ix, int iy, int jx, int jy)
+    {
         T temp = array[iy][ix];
         array[iy][ix] = array[jy][jx];
         array[jy][jx] = temp;
