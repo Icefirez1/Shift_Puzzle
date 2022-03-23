7e78a5e815fcb3601f57b6fb8d722c03785ff35a
lucror1 <paulw2022@gmail.com>
2022-03-23 13:55:15 -0400

    Added Button extension and position

--- Tile.java
+++ Tile.java
@@ -1,10 +1,23 @@
-public class Tile
+import javafx.scene.control.Button;
+
+public class Tile extends Button
 {
     private Integer num;
+    private int x;
+    private int y;
+
     public Tile(Integer num)
     {
-        this.num = num; 
+        this(num, 0, 0);
     }
+
+    public Tile(Integer num, int x, int y)
+    {
+        this.num = num;
+        this.x = x;
+        this.y = y;
+    }
+
     public int value()
     {
         return num; 
